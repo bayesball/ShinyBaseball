6f568720f74e2dc14bc4f67996730411b7555846
Jim Albert <albertcb1@gmail.com>
2021-02-27 19:38:57 -0500

    removed several shiny apps

--- R/PitchCount.R
+++
@@ -1,9 +0,0 @@
-PitchCount <- function() {
-  appDir <- system.file("shiny-examples", "PitchCount",
-                        package = "ShinyBaseball")
-  if (appDir == "") {
-    stop("Could not find example directory. Try re-installing `TeachBayes`.", call. = FALSE)
-  }
-
-  shiny::runApp(appDir, display.mode = "normal")
-}
--- R/PitchCount2.R
+++
@@ -1,9 +0,0 @@
-PitchCount2 <- function() {
-  appDir <- system.file("shiny-examples", "PitchCount2",
-                        package = "ShinyBaseball")
-  if (appDir == "") {
-    stop("Could not find example directory. Try re-installing `TeachBayes`.", call. = FALSE)
-  }
-
-  shiny::runApp(appDir, display.mode = "normal")
-}
