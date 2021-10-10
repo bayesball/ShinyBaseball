d303f78f3138a57a73bc44039fbe3169abcb8dad
bayesball <albertcb1@gmail.com>
2021-10-09 20:34:12 -0400

    adding vignette

--- inst/shiny-examples/PitcherFourSeam/app.R
+++ inst/shiny-examples/PitcherFourSeam/app.R
@@ -226,7 +226,7 @@ ui <- fluidPage(
                 value = "Aaron Nola"),
       checkboxGroupInput(
         "year",
-        "Select Seasons",
+        "Select Seasons:",
         choices = c("2015", "2016", "2017",
                     "2018", "2019", "2020", "2021"),
         selected = c("2015", "2016", "2017",
