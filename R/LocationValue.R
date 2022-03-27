7e7c1327b06f16d2aa5f96718a124df5d0115957
bayesball <albertcb1@gmail.com>
2022-03-26 21:11:25 -0400

    add files

--- R/LocationValue.R
+++
@@ -1,9 +0,0 @@
-LocationValue <- function() {
-  appDir <- system.file("shiny-examples", "LocationValue",
-                        package = "ShinyBaseball")
-  if (appDir == "") {
-    stop("Could not find example directory. Try re-installing `TeachBayes`.", call. = FALSE)
-  }
-
-  shiny::runApp(appDir, display.mode = "normal")
-}
