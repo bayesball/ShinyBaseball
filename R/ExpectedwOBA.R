cef55144ccd97d6d034887249da9552b7a1c815e
bayesball <albertcb1@gmail.com>
2022-10-26 16:54:14 -0400

    add man items

---
+++ R/ExpectedwOBA.R
@@ -0,0 +1,10 @@
+ExpectedwOBA <- function() {
+  appDir <- system.file("shiny-examples",
+                        "ExpectedwOBA",
+                        package = "ShinyBaseball")
+  if (appDir == "") {
+    stop("Could not find example directory. Try re-installing `TeachBayes`.", call. = FALSE)
+  }
+
+  shiny::runApp(appDir, display.mode = "normal")
+}
