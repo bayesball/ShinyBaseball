604c491107c108469fbf97d9712ed5fb50577a5f
bayesball <albertcb1@gmail.com>
2024-01-27 10:41:58 -0500

    revise app

--- inst/shiny-examples/BattingProfile/app.R
+++ inst/shiny-examples/BattingProfile/app.R
@@ -14,9 +14,6 @@ batting_profile <- function(sc_ip, batter_no,
   require(purrr)
   require(stringr)
 
-  logit <- function(y){
-    log(y) - log(1 - y)
-  }
   bin_rates <- function(sc_ip, phi1_breaks, LA_breaks,
                         name = "") {
     Total_BIP <- nrow(sc_ip)
@@ -36,10 +33,8 @@ batting_profile <- function(sc_ip, batter_no,
       )
   }
   bin_plot_label <- function(S, P_breaks, LA_breaks,
-                             name = "") {
-    logit <- function(y){
-      log(y) - log(1 - y)
-    }
+                             measure, name = "",
+                             stitle = "") {
     compute_bin_midpoint <- function(x) {
       x |>
         as.character() |>
@@ -47,15 +42,30 @@ batting_profile <- function(sc_ip, batter_no,
         map_dbl(parse_number) |>
         mean()
     }
+    if(measure == "Dlogit"){
+      S |>
+        mutate(Measure = Dlogit) -> S
+      rounding <- 1
+    }
+    if(measure == "Z"){
+      S |>
+        mutate(Measure = Z) -> S
+      rounding <- 1
+    }
+    if(measure == "Diff"){
+      S |>
+        mutate(Measure = Diff) -> S
+      rounding <- 0
+    }
     S |>
       mutate(
         ph = map_dbl(PH, compute_bin_midpoint),
         la = map_dbl(LA, compute_bin_midpoint),
-        Sign = ifelse(Dlogit > 0, "pos", "neg"),
-        DL = round(Dlogit, 1)
+        Sign = ifelse(Measure > 0, "pos", "neg"),
+        Measure_display = round(Measure, rounding)
       ) |>
       ggplot(aes(x = ph, y = la)) +
-      geom_text(aes(label = DL, color = Sign),
+      geom_text(aes(label = Measure_display, color = Sign),
                 size = 6) +
       geom_vline(
         xintercept = P_breaks,
@@ -68,11 +78,16 @@ batting_profile <- function(sc_ip, batter_no,
       theme(text = element_text(size = 18)) +
       labs(x = "Adjusted Spray Angle (degrees)",
            y = "Launch Angle (degrees)",
-           title = paste("Comparative Batting Profile of", Name)) +
+           title = paste("Comparative Batting Profile of", name),
+           subtitle = stitle) +
       theme(plot.title = element_text(colour = "blue", size = 16,
+                      hjust = 0.5, vjust = 0.8, angle = 0)) +
+      theme(plot.subtitle = element_text(colour = "red", size = 16,
                                       hjust = 0.5, vjust = 0.8, angle = 0))
   }
-  bin_plot_color <- function(S, P_breaks, LA_breaks) {
+
+  bin_plot_color <- function(S, P_breaks, LA_breaks, name = "",
+                             stitle = "") {
     compute_bin_midpoint <- function(x) {
       x |>
         as.character() |>
@@ -86,7 +101,7 @@ batting_profile <- function(sc_ip, batter_no,
         la = map_dbl(LA, compute_bin_midpoint)
       ) |>
       ggplot(aes(x = ph, y = la)) +
-      geom_tile(aes(fill = D_LOGIT)) +
+      geom_tile(aes(fill = Dlogit)) +
       geom_vline(
         xintercept = P_breaks,
         color = "blue"
@@ -98,13 +113,16 @@ batting_profile <- function(sc_ip, batter_no,
       theme(text = element_text(size = 18)) +
       labs(x = "Adjusted Spray Angle (degrees)",
            y = "Launch Angle (degrees)",
-           title = paste("Comparative Batting Profile of", Name)) +
+           title = paste("Comparative Batting Profile of", name),
+           subtitle = stitle) +
       #    theme(legend.position = "none") +
       scale_fill_gradientn(colours = colorspace::diverge_hcl(7)) +
       theme(plot.title = element_text(colour = "blue", size = 16,
-                                      hjust = 0.5, vjust = 0.8, angle = 0))
+                        hjust = 0.5, vjust = 0.8, angle = 0)) +
+      theme(plot.subtitle = element_text(colour = "red", size = 16,
+                        hjust = 0.5, vjust = 0.8, angle = 0))
   }
-  bin_plot_scatter <- function(ff, P_breaks, LA_breaks) {
+  bin_plot_scatter <- function(ff, P_breaks, LA_breaks, name) {
     ggplot(ff, aes(x = phi1, y = launch_angle)) +
       geom_point() +
       geom_vline(
@@ -118,14 +136,15 @@ batting_profile <- function(sc_ip, batter_no,
       theme(text = element_text(size = 18)) +
       labs(x = "Adjusted Spray Angle (degrees)",
            y = "Launch Angle (degrees)",
-           title = paste("Batting Profile of", Name)) +
+           title = paste("Batting Profile of", name)) +
       #    theme(legend.position = "none") +
       scale_fill_gradientn(colours = colorspace::diverge_hcl(7)) +
       theme(plot.title = element_text(colour = "blue", size = 18,
                                       hjust = 0.5,
                                       vjust = 0.8, angle = 0))
   }
-  bin_plot <- function(S, PH_breaks, LA_breaks, label) {
+  bin_plot <- function(S, PH_breaks, LA_breaks, label, name,
+                       stitle = ""){
     compute_bin_midpoint <- function(x) {
       x |>
         as.character() |>
@@ -151,12 +170,18 @@ batting_profile <- function(sc_ip, batter_no,
       theme(text = element_text(size = 18)) +
       labs(x = "Adjusted Spray Angle (degrees)",
            y = "Launch Angle (degrees)",
-           title = paste("Batting Profile of", Name)) +
+           title = paste("Batting Profile of", name),
+           subtitle = stitle) +
       theme(plot.title = element_text(colour = "blue", size = 18,
                                       hjust = 0.5,
-                                      vjust = 0.8, angle = 0))
-  }
+                                      vjust = 0.8, angle = 0)) +
+      theme(plot.subtitle = element_text(colour = "red", size = 18,
+                        hjust = 0.5, vjust = 0.8, angle = 0))
+    }
 
+  logit <- function(y){
+    log(y) - log(1 - y)
+  }
   # 6, 9, 12
   p_breaks <- seq(-50, 50, length.out = n_bins + 1)
   la_breaks <- seq(-50, 70, length.out = n_bins + 1)
@@ -164,6 +189,7 @@ batting_profile <- function(sc_ip, batter_no,
   # binning
   out <- bin_rates(sc_ip, p_breaks, la_breaks)
   ff <- filter(sc_ip, batter == batter_no)
+  BIP_player <- dim(ff)[1]
   Name <- ff$player_name[1]
   out_ff <- bin_rates(ff, p_breaks, la_breaks)
 
@@ -174,16 +200,29 @@ batting_profile <- function(sc_ip, batter_no,
   # difference in logits measure
   out_new <- mutate(out_new,
                     Dlogit = logit(BIP_Rate.y) -
-                      logit(BIP_Rate.x))
+                      logit(BIP_Rate.x),
+                    Diff = BIP.y -
+                      round(BIP_Rate.x * BIP_player),
+                    E = BIP_Rate.x * BIP_player,
+                    Z = round(Diff / sqrt(E), 1))
 
-  p0 <- bin_plot_scatter(ff, p_breaks, la_breaks)
-  p0a <- bin_plot(out_ff, p_breaks, la_breaks, BIP)
-  p1 <- bin_plot_label(out_new, p_breaks, la_breaks)
-  out_new <- mutate(out_new,
-                    D_LOGIT = round(Dlogit, 1))
-  p2 <- bin_plot_color(out_new, p_breaks, la_breaks)
+  p0 <- bin_plot_scatter(ff, p_breaks, la_breaks, Name)
+  p0a <- bin_plot(out_ff, p_breaks, la_breaks, BIP, Name,
+                  stitle = "Balls in Play")
+  p0b <- bin_plot_label(out_new, p_breaks, la_breaks,
+                        "Diff", Name,
+                  stitle = "BIP Minus BIP Average")
+  p0c <- bin_plot_label(out_new, p_breaks, la_breaks,
+                        "Z", Name,
+                  stitle ="Z Score")
+  p1 <- bin_plot_label(out_new, p_breaks, la_breaks,
+                       "Dlogit", Name,
+                  stitle = "Difference on Logit Scale")
+  p2 <- bin_plot_color(out_new, p_breaks, la_breaks, Name,
+                  stitle = "Difference on Logit Scale")
 
-  list(p0 = p0, p0a = p0a, p1 = p1, p2 = p2)
+  list(p0 = p0, p0a = p0a, p0b = p0b,
+       p0c = p0c, p1 = p1, p2 = p2)
 }
 
 ui <- fluidPage(
@@ -202,6 +241,8 @@ ui <- fluidPage(
       radioButtons("type", "Choose what to display:",
                    c("Scatterplot" = "scatter",
                      "Bin Counts" = "bin_counts",
+                     "Difference in Counts" = "dcount",
+                     "Z Scores" = "zscore",
                      "Difference in Logits" = "dlogit",
                      "Tile Graph of Difference" = "tile")),
       radioButtons("n_bins", "Choose number of bins:",
@@ -212,7 +253,7 @@ ui <- fluidPage(
                      "10" = "10"),
                    inline = TRUE),
       hr(),
-      p("Comparative Batting Profile gives for each bin the logit(BIP rate) of the player minus the logit(BIP rate) for all players in the 2023 season.")
+      p("Comparative Batting Profile compares for each bin the BIP of the player minus the BIP for all players using different scales.")
     ),
     column(
       9,
@@ -234,6 +275,10 @@ ui <- fluidPage(
           OUT$p0
         } else if (input$type == "bin_counts") {
           OUT$p0a
+        } else if (input$type == "dcount") {
+          OUT$p0b
+        } else if (input$type == "zscore") {
+          OUT$p0c
         } else if (input$type == "dlogit") {
           OUT$p1
         } else {
